1bd460188eca71fd3e94d1dc3c10c027fd6975e7
Luke Bovard <lukebovard@gmail.com>
2013-01-30 11:31:07 -0500

    generate plots

--- omega_plots.m
+++ omega_plots.m
@@ -2,13 +2,16 @@ close all;
 clear all;
 N=512;
 
-%kz=[120,80,50,25];
-kz=50*ones(4,1); 
-Re=[20000,10000,5000,2000];
-vtimes=[11,11,6,3];
-Fh=0.2;
-for i=1:4
-[om{i},ptitle{i}]=omega_information(kz(i),Fh,Re(i),N,vtimes(i),0);
+%second peak
+%kz=[50,60,70,40,50,55,30,35,45]; 
+
+%oscillation
+kz=[8,20,40,8,20,40,8,20,40];
+Re=[20000,20000,20000,10000,10000,10000,5000,5000,5000];
+vtimes=[12,5,6,5,5,6,5,5,6];
+Fh=[0.2,0.1,0.05,0.2,0.1,0.05,0.2,0.1,0.05];
+for i=1:9
+[om{i},ptitle{i}]=omega_information(kz(i),Fh(i),Re(i),N,vtimes(i),0);
 end
 
 L=9;dx=L/N;
@@ -16,8 +19,8 @@ x=-L/2+dx*(1:N);
 y=x';
 [X,Y]=meshgrid(x,y);
 
-for i=1:4
-	subplot(2,2,i)
+for i=1:9   
+	subplot(3,3,i)
 	surf(X,Y,om{i},'EdgeColor','none')
 	axis([-1.2 1.2 -1.2 1.2])
 	axis square
